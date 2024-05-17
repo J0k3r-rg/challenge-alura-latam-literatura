ba5e78c1d735c0f03d08eb6e76fee47676a330eb
j0k3r <pdasriogallegos@gmail.com>
2024-05-17 14:36:26 -0300

    configs initials

---
+++ src/main/java/com/j0k3r/challengealuralatamliteratura/ChallengeAluraLatamLiteraturaApplication.java
@@ -0,0 +1,13 @@
+package com.j0k3r.challengealuralatamliteratura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAluraLatamLiteraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAluraLatamLiteraturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j0k3r/challengealuralatamliteratura/ChallengeAluraLatamLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j0k3r.challengealuralatamliteratura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAluraLatamLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
